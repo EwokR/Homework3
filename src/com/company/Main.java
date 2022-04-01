2f3e0993b518464bb64b1710ce8b1ac8c71bbcf9
Anton <imewokrus@gmail.com>
2022-04-01 14:36:39 +0300

    Third commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,15 +6,15 @@ public class Main {
 
     public static void main(String[] args) {
         // Упражнение №1
-        int clientOS = 0;
-        if (clientOS <= 1) {
+        int clientOS = 1;
+        if (clientOS < 1) {
             System.out.println("Download Android app from this link.");
         } else {
             System.out.println("Download IOS app from this link.");
         }
         // Упражнение №2
-        int clientDeviceYear = 2014;
-        if (clientOS <= 1 && clientDeviceYear < 2015) {
+        int clientDeviceYear = 2018;
+        if (clientOS == 1 && clientDeviceYear < 2015) {
             System.out.println("Download light Android app from this link.");
         } else if (clientOS == 0 && clientDeviceYear < 2015) {
             System.out.println("Download light IOS app from this link.");
@@ -35,11 +35,11 @@ public class Main {
             System.out.println(year + " this year is not interalary year.");
         // Задание №4
         int deliveryDistance = 95;
-        if (deliveryDistance <=20)
+        if (deliveryDistance <= 20)
             System.out.println("Cart arrive next day.");
-        else if (deliveryDistance >= 20 || deliveryDistance <=60)
+        else if (deliveryDistance >= 20 || deliveryDistance <= 60)
             System.out.println("Cart arrive after two days.");
-        else if (deliveryDistance >=60 || deliveryDistance <=100)
+        else if (deliveryDistance >= 60 || deliveryDistance <= 100)
             System.out.println("Cart arrive after three days.");
         else
             System.out.println("Cart arrive on a work week.");
@@ -69,8 +69,35 @@ public class Main {
             default:
                 System.out.println("This season does not exist");
         }
+        // Задание №6
+        {
+            int a = 12;
+            int b = 27;
+            int c = 44;
+            int d = 15;
+            int e = 9;
+            int aBCDE = a * (b + (c - d * e));
+            int result = -aBCDE;
+            System.out.println(result);
+        }
+        // Задание №7
+        {
+            int a = 5;
+            int b = 7;
+            a = a + b - (b = a);
+            System.out.println(a);
+            System.out.println(b);
+        }
+        // Задание №8
+        int a = 456;
+        int b =1;
+        b = a / 10;
+        b = b % 10;
+        System.out.println(b);
+
     }
 }
+
 
 
 
